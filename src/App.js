2bf10399b5ae41b240714ad8cdd5a5f8b12c3205
Harsha <hvgnani79813@gmail.com>
2024-05-19 19:34:46 +0900

    update app

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   
     <div className="mainpage">
 
-     <Routes>
+     <Routes basename="/personalportfolio">
        <Route path="/" element={<Home/>}/>
        <Route path="/about" element={<About/>}/>
        <Route path="/projects" element={<Projects/>}/>
